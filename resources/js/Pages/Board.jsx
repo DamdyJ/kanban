31b1e010b5c7f3e0d394f0f75059f3ce2c868287
Damdy <damdyjuns@gmail.com>
2024-06-23 18:36:55 +0700

    code cleanup and refactor

--- resources/js/Pages/Board.jsx
+++ resources/js/Pages/Board.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import KanbanBoard from "@/Components/KanbanBoard";
 
 export default function Board() {
   return (
-    <div>Board</div>
-  )
+    <>
+      <div className="flex h-screen w-full flex-col overflow-hidden bg-gray-950">
+        <header className="flex h-16 w-full shrink-0 items-center justify-between bg-gray-800 px-4 py-2 text-white">
+          <a href="/" aria-label="logo" className="text-2xl font-bold">
+            Kanban
+          </a>
+          <nav>
+            <ul>
+              <li>
+                <a href="/app"></a>
+              </li>
+              <li>
+                <img
+                  src="https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="profile"
+                  className="h-11 w-11 rounded-full object-cover object-top ring-2 ring-offset-2"
+                />
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="flex max-h-[calc(100vh-4rem)] items-center justify-center overflow-y-auto">
+          <KanbanBoard />
+        </main>
+      </div>
+    </>
+  );
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,18 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
-
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import KanbanBoard from "@/Components/KanbanBoard";
+import { Head } from "@inertiajs/react";
 export default function Dashboard({ auth }) {
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+    >
+      <Head title="Dashboard" />
+      <main className="bg-neutral-900">
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+      <div className="flex max-h-[calc(100vh-4rem)] w-full max-w-7xl mx-auto items-center justify-center overflow-y-auto">
+        <KanbanBoard />
+      </div>
+      </main>
+    </AuthenticatedLayout>
+  );
 }
